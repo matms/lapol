20f04bf6fd4869812a23edebd30be82b842cdee6
matms <matm31415@gmail.com>
2021-04-29 21:02:29 -0400

    Rename 'define' to 'commands' in lapol modules.

--- src/default_lapol_modules/main.ts
+++ src/default_lapol_modules/main.ts
@@ -1,4 +1,4 @@
-export const define = {
+export const commands = {
     hey: hey,
 };
 
--- src/la_module/mod_utils.ts
+++ src/la_module/mod_utils.ts
@@ -1,18 +1,20 @@
 import { functionToCommand } from "../command/function_to_command";
 import { LapolModuleError } from "../errors";
 
+const LA_MOD_EXPORT_COMMAND_NAME = "commands";
+
 /** Load module, return map with its contents. Internal use function (in the future,
  * a better function will be provided that also returns module metadata). */
 export async function loadLapolModAsMap(modPath: string): Promise<Map<string, any>> {
     let mod = await import(modPath);
     let map = new Map();
 
-    if (!mod.hasOwnProperty("define")) {
-        throw new LapolModuleError("No define propriety in module");
+    if (!mod.hasOwnProperty(LA_MOD_EXPORT_COMMAND_NAME)) {
+        throw new LapolModuleError(`No ${LA_MOD_EXPORT_COMMAND_NAME} propriety in module`);
     }
 
-    for (let prop of Object.getOwnPropertyNames(mod.define)) {
-        let val = mod.define[prop];
+    for (let prop of Object.getOwnPropertyNames(mod[LA_MOD_EXPORT_COMMAND_NAME])) {
+        let val = mod[LA_MOD_EXPORT_COMMAND_NAME][prop];
         if (typeof val === "function") {
             map.set(prop, functionToCommand(val, prop));
         } else {
