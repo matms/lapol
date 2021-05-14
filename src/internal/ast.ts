20beeedbac020648ea37cc4f54df85106b6b80eb
matms <matm31415@gmail.com>
2021-05-14 00:13:05 -0400

    Manually made typescript definitions for rust AST.

--- src/internal/ast.ts
+++ src/internal/ast.ts
@@ -1,26 +1,71 @@
+//! These are _manually_ generated from rust
+//! See lapol-parse-rs file ast.rs for details on these types.
+
+export interface AstTextNode {
+    t: "AstTextNode";
+    content: string;
+}
+
+export interface AstCommandNode {
+    t: "AstCommandNode";
+    commandName: string;
+    squareArgs: SquareArg[] | null;
+    curlyArgs: AstNode[][];
+}
+
+export interface AstRootNode {
+    t: "AstRootNode";
+    subNodes: AstNode[];
+}
+
 export enum AstNodeKind {
     AstStrNode = "AstStrNode",
     AstCommandNode = "AstCommandNode",
     AstRootNode = "AstRootNode",
 }
 
-export interface AstStrNode {
-    kind: AstNodeKind.AstStrNode;
-    content: string;
-    sourceStartCol: number; // The first column is 1.
-    sourceStartLine: number; // The first line is 1.
+export interface SquareArgVal {
+    t: "Val";
+    c: SquareEntry;
 }
 
-export interface AstCommandNode {
-    kind: AstNodeKind.AstCommandNode;
-    commandName: string;
-    squareArg: AstNode[] | undefined;
-    curlyArgs: AstNode[][];
+export interface SquareArgKeyVal {
+    t: "Val";
+    c: SquareEntry[];
 }
 
-export interface AstRootNode {
-    kind: AstNodeKind.AstRootNode;
-    subNodes: AstNode[];
+export interface SquareEntryNum {
+    t: "Num";
+    c: number;
 }
 
-export type AstNode = AstStrNode | AstCommandNode | AstRootNode;
+export interface SquareEntryIdent {
+    t: "Ident";
+    c: string;
+}
+
+export interface SquareEntryBool {
+    t: "Bool";
+    c: boolean;
+}
+
+export interface SquareEntryQuotedStr {
+    t: "QuotedStr";
+    c: string;
+}
+
+export interface SquareEntryAstNode {
+    t: "AstNode";
+    c: AstNode;
+}
+
+export type SquareEntry =
+    | SquareEntryNum
+    | SquareEntryIdent
+    | SquareEntryBool
+    | SquareEntryQuotedStr
+    | SquareEntryAstNode;
+
+export type SquareArg = SquareArgVal | SquareArgKeyVal;
+
+export type AstNode = AstTextNode | AstCommandNode | AstRootNode;
