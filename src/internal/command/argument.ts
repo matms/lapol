a332f210b21eae67196c296df330660875a859e1
matms <matm31415@gmail.com>
2021-05-29 23:06:05 -0400

    Added *OrErr() functions to CommandArgument. Made CommandArgument into abstract class.

--- src/internal/command/argument.ts
+++ src/internal/command/argument.ts
@@ -1,34 +1,65 @@
 import { DetNode } from "../det";
+import { LapolError } from "../errors";
 
 export type ArgumentEvaluationStrategy = "eager" | "lazy";
 
 export type CmdSquareArg = number | string | boolean | DetNode;
 export type CmdCurlyArg = DetNode[];
 
-export interface CommandArguments {
-    evaluation: ArgumentEvaluationStrategy;
+export abstract class CommandArguments {
+    abstract readonly evaluation: ArgumentEvaluationStrategy;
 
     /** (Get) array of Curly Arguments */
-    cas: () => CmdCurlyArg[];
+    public abstract cas(): CmdCurlyArg[];
     /** (Get) array of Square Arguments */
-    sas: () => CmdSquareArg[];
+    public abstract sas(): CmdSquareArg[];
     /** (Get) map of KeyWord Arguments */
-    kwas: () => Map<string, CmdSquareArg>;
+    public abstract kwas(): Map<string, CmdSquareArg>;
 
     /** (Get) Curly Argument by index */
-    ca: ((idx: number, _default?: undefined) => CmdCurlyArg | undefined) &
-        ((idx: number, _default: CmdCurlyArg) => CmdCurlyArg);
+    public abstract ca(idx: number, _default?: undefined): CmdCurlyArg | undefined;
+    public abstract ca(idx: number, _default: CmdCurlyArg): CmdCurlyArg;
 
     /** (Get) Square Argument by index */
-    sa: ((idx: number, _default?: undefined) => CmdSquareArg | undefined) &
-        ((idx: number, _default: CmdSquareArg) => CmdSquareArg);
+    public abstract sa(idx: number, _default?: undefined): CmdSquareArg | undefined;
+    public abstract sa(idx: number, _default: CmdSquareArg): CmdSquareArg;
 
     /** (Get) KeyWord Argument by index */
-    kwa: ((idx: string, _default?: undefined) => CmdSquareArg | undefined) &
-        ((idx: string, _default: CmdSquareArg) => CmdSquareArg);
+    public abstract kwa(idx: string, _default?: undefined): CmdSquareArg | undefined;
+    public abstract kwa(idx: string, _default: CmdSquareArg): CmdSquareArg;
+
+    /** (Get) Curly Argument by index. Throws an error if the given argument does not exist. */
+    public caOrErr(idx: number): CmdCurlyArg {
+        const o = this.ca(idx);
+        if (o === undefined)
+            throw new LapolError(
+                `caOrErr(${idx}) -> Curly argument undefined (Likely not provided).`
+            );
+        return o;
+    }
+
+    /** (Get) Square Argument by index. Throws an error if the given argument does not exist. */
+    public saOrErr(idx: number): CmdSquareArg {
+        const o = this.sa(idx);
+        if (o === undefined)
+            throw new LapolError(
+                `saOrErr(${idx}) -> Square argument undefined (Likely not provided).`
+            );
+        return o;
+    }
+
+    /** (Get) KeyWord Argument by index. Throws an error if the given argument does not exist. */
+    public kwaOrErr(idx: string): CmdSquareArg {
+        const o = this.kwa(idx);
+        if (o === undefined)
+            throw new LapolError(
+                `kwaOrErr(${idx}) -> Keyword argument undefined (Likely not provided).`
+            );
+        return o;
+    }
 }
 
-export class EagerCommandArguments implements CommandArguments {
+export class EagerCommandArguments extends CommandArguments {
     readonly evaluation: ArgumentEvaluationStrategy = "eager";
     readonly _curlyArgs: CmdCurlyArg[];
     readonly _squareArgs: CmdSquareArg[];
@@ -39,6 +70,7 @@ export class EagerCommandArguments implements CommandArguments {
         squareArgs: CmdSquareArg[],
         curlyArgs: CmdCurlyArg[]
     ) {
+        super();
         this._keywordArgs = keywordArgs;
         this._squareArgs = squareArgs;
         this._curlyArgs = curlyArgs;
@@ -85,4 +117,4 @@ export class EagerCommandArguments implements CommandArguments {
 }
 
 // TODO: LazyCommandArgs -> Evaluate arg only upon request.
-// export class LazyCommandArgs implements CommandArguments {}
+// export class LazyCommandArgs extends CommandArguments {}
